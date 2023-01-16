9f848458e31b81f354e2707e0c1745f409015dfb
JulianPetersen <julianpetersen92@gmail.com>
2023-01-16 11:51:25 -0300

    primer push admin de shorti

---
+++ src/app/components/componentmodule.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports:[
+    MenuComponent
+  ]
+})
+export class ComponentmoduleModule { }
